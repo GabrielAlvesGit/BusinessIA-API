af083de90cec043d5e3e86594c86f2633f480c76
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-08-24 16:19:26 -0300

    Fix

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// <! ----- IGNORE - Rotas de teste --- -->
-// Route::get('/users', [UserController::class, 'index']);
-// Route::post('/users', [UserController::class, 'store']);
-
 Route::apiResource('/users', UserController::class);
