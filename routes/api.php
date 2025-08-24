c2a095b5ef178ef54bf3dc6c34f54d9521385232
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-08-24 17:16:56 -0300

    Fix: Vercel v.5

--- routes/api.php
+++ routes/api.php
@@ -1,41 +1,6 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::apiResource('users', UserController::class);
-
-Route::get('/test-vercel', function () {
-    return [1, 3, 5];
-});
-
-
-Route::get('/test-db-con', function () {
-    try {
-        $dbconnect = DB::connection()->getPDO();
-        $dbname = DB::connection()->getDatabaseName();
-        echo "Connected successfully to the database. Database name is :" . $dbname;
-    } catch (Exception $e) {
-        echo "Error in connecting to the database". $e->getMessage();
-    }
-});
+Route::apiResource('/users', UserController::class);
