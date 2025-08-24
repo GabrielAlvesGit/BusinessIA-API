a4f24c7cf46cf25908687c1f690d97e665dac753
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-08-24 17:33:08 -0300

    Fix: Vercel v.5

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,4 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Use a group to apply the /api prefix to all your routes
-Route::prefix('api')->group(function () {
-    Route::get('/users', [UserController::class, 'index']);
-});
+Route::get('/users', [UserController::class, 'index']);
