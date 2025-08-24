392470eb3296af60d18643b44badc655edb989cd
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-08-24 12:42:53 -0300

    Fix: Vercel V.2

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,44 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-// <! ----- IGNORE - Rotas de teste --- -->
-// Route::get('/users', [UserController::class, 'index']);
-// Route::post('/users', [UserController::class, 'store']);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-// Route::apiResource('/users', UserController::class);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 Route::apiResource('users', UserController::class);
+Route::apiResource('orders', OrderController::class);
+Route::apiResource('products', ProductController::class);
+Route::apiResource('categories', CategoryController::class);
+
+Route::get('/test-vercel', function () {
+    return [1, 3, 5];
+});
+
+
+Route::get('/test-db-con', function () {
+    try {
+        $dbconnect = DB::connection()->getPDO();
+        $dbname = DB::connection()->getDatabaseName();
+        echo "Connected successfully to the database. Database name is :" . $dbname;
+    } catch (Exception $e) {
+        echo "Error in connecting to the database". $e->getMessage();
+    }
+});
