fce6d81352317c81722e018b6a55f06ba56bfd57
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-08-25 10:53:28 -0300

    Fix:

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,4 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/users', [UserController::class, 'index']);
 // Route::post('/users', [UserController::class, 'store']);
 
-Route::apiResource('/users', UserController::class);
+Route::apiResource('users', UserController::class);
